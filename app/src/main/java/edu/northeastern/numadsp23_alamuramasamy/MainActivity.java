f8702d7b529d1795a0d73b8766c223b5656c31c1
Alamu Ramasamy <alamuramasamy12@gmail.com>
2023-01-15 02:04:52 -0500

    button and toast

--- app/src/main/java/edu/northeastern/numadsp23_alamuramasamy/MainActivity.java
+++ app/src/main/java/edu/northeastern/numadsp23_alamuramasamy/MainActivity.java
@@ -3,12 +3,25 @@ package edu.northeastern.numadsp23_alamuramasamy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAbtMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbtMe = findViewById(R.id.buttonAbtMe);
+        buttonAbtMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Name: Alamu Ramasamy, \nEmail: ramasamy.a@northeaastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

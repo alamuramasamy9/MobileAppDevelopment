36e67ec4b87171baa714e41649625ffec8fd41e3
Alamu Ramasamy <alamuramasamy12@gmail.com>
2023-01-26 23:01:08 -0500

    button clicky

--- app/src/main/java/edu/northeastern/numadsp23_alamuramasamy/MainActivity.java
+++ app/src/main/java/edu/northeastern/numadsp23_alamuramasamy/MainActivity.java
@@ -2,6 +2,7 @@ package edu.northeastern.numadsp23_alamuramasamy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     Button buttonAbtMe;
+    Button buttonClicky;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"Name: Alamu Ramasamy, \nEmail: ramasamy.a@northeaastern.edu", Toast.LENGTH_LONG).show();
             }
+
         });
+
+        buttonClicky = findViewById(R.id.buttonClicky);
+
+        buttonClicky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  openClicky();
+            }
+        });
+    }
+
+    public void openClicky(){
+        Intent intent = new Intent(this, ClickyActivity.class);
+        startActivity(intent);
     }
 }
